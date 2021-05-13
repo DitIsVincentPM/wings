e2c8c6995df48b63467e7779736600ab7516e653
DitIsVincentPM <69402451+DitIsVincentPM@users.noreply.github.com>
2021-05-13 23:04:30 +0200

    Update docker.bash

--- docker.bash
+++ docker.bash
@@ -1,1 +1,11 @@
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -qq && curl -fsSL https://download.docker.com/linux/ubuntu/gpg &&  sudo apt-key add gpg && apt-get update && apt-get -y install docker-ce=5:19.03.14~3-0~ubuntu-bionic docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic containerd.io=1.3.9-1 && systemctl enable docker
+sudo apt-get update
+sudo apt-get -y install curl
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -qq
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg && sudo apt-key add gpg && apt-get update 
+apt-get -y install docker-ce=5:19.03.14~3-0~ubuntu-bionic docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic containerd.io=1.3.9-1
+systemctl enable docker
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
+chmod u+x /usr/local/bin/wings
+curl -L -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/DitIsVincentPM/wings/main/wings.service
+systemctl enable --now wings
